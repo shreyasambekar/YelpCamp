86c2462a9995dd9a3bcfce8492dadb0f65563ca8
shreyasambekar <shreyasambekar2@gmail.com>
2021-05-18 23:15:12 +0530

    Connected to production database on MongoDB hosted on Mongo Atlas, used connect-mongo package for using MongoDB as session store instead of default memory store for storing session information

--- app.js
+++ app.js
@@ -18,9 +18,13 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
 const mongoSanitize = require('express-mongo-sanitize')
-const helmet = require('helmet')
+const helmet = require('helmet');
+const MongoDBStore = require("connect-mongo")
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+
+
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -42,9 +46,24 @@ app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize())
 
+const secret = process.env.SECRET || 'thisnotthewaytosetsecret'
+
+const store = new MongoDBStore({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret
+    },
+    touchAfter: 24 * 60 * 60,
+})
+
+store.on("error", function (e) {
+    console.log("Session Store error", e)
+})
+
 const sessionConfig = {
+    store,
     name: 'yelpcampsession',
-    secret: 'thisnotthewaytosetsecret',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
